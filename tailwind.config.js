c7c10d89cab95941561a13817c1c0690390dfae1
SHOVON <141106301+shopnilshovon@users.noreply.github.com>
2025-07-31 18:41:33 +0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  darkMode: 'class', // For Light/Dark Mode
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

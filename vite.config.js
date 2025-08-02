03006122ce5a9b1e88e0be652fb6546c159615e0
SHOVON <141106301+shopnilshovon@users.noreply.github.com>
2025-08-02 16:40:36 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path'; // Required for alias configuration
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // Now "@/components/..." points to "src/components/..."
+    },
+  },
 });
